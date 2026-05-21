7a87ea3251cb6330d6d89429d048bb77739a5845
KeralaTimetable <sudhasangeethveena@gmail.com>
2026-05-21 21:35:48 +0530

    Update poll configuration for subjects and tags

--- poll-config.js
+++ poll-config.js
@@ -1,18 +1,16 @@
 // poll-config.js
-// Make sure this file name exactly matches the script tag in polls.html
-
 window.examPollConfig = [
     {
         id: "UCEST105_2026_Regular", 
-        subjectName: "UCEST105 - Python for Engineers",
-        type: "Regular",
+        subjectName: "UCEST105 - Python",
+        tags: ["CSE", "S1", "Regular"], // 👈 Add as many tags as you want here!
         opensAt: "2026-05-21T12:30:00", 
         closesAt: "2026-05-22T12:30:00"  
     },
     {
-        id: "GAMAT101_2026_Regular",
+        id: "GAMAT101_2026_Supply",
         subjectName: "GAMAT101 - Linear Algebra",
-        type: "Regular",
+        tags: ["Common", "S1", "Supply"], 
         opensAt: "2026-05-24T12:30:00",
         closesAt: "2026-05-25T12:30:00"
     }
