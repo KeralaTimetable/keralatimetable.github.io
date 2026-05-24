78fdec44c2b865d733b3cf0c674a574e90fdd0f2
KeralaTimetable <sudhasangeethveena@gmail.com>
2026-05-24 11:24:22 +0530

    Update S3 timetable start and end dates

---
+++ timetable-config.js
@@ -0,0 +1,41 @@
+// timetable-config.js
+// Date format MUST be: "YYYY-MM-DDTHH:MM:SS" (24-hour time format)
+
+window.timetablesData = [
+    {
+        id: "s1_supply_may2026",
+        title: "B.Tech S1 (S) Examination May 2026",
+        semester: "S1",
+        categoryBadge: "S1/S2", 
+        type: "Supply only",
+        scheme: "2024 Scheme",
+        startDate: "2026-05-18T13:30:00", 
+        endDate: "2026-05-29T16:30:00",   
+        viewLink: "btech/s2/supply-2026.html",
+        pdfLink: "https://drive.google.com/file/d/1_2FuhpYLlEZsdYii8wzncvUOSdjuz_kI/view?usp=drivesdk"
+    }, 
+    {
+        id: "s3_supply_june2026",
+        title: "B.Tech S3 (S, FE) Examination June 2026",
+        semester: "S3",
+        categoryBadge: "S3/S4", 
+        type: "Supply & FE",
+        scheme: "2019 Scheme",
+        startDate: "2026-06-02T13:30:00", // Update with exact S3 dates
+        endDate: "2026-06-17T16:30:00",   // Update with exact S3 dates
+        viewLink: "btech/s3/supply-2026.html",
+        pdfLink: "https://drive.google.com/file/d/1mdHPCTodfVSTSs9In3EkP54FgczqvExv/view?usp=drivesdk"
+    },
+    {
+        id: "s4_supply_may2026",
+        title: "B.Tech S4 (S, FE) Examination May 2026",
+        semester: "S4",
+        categoryBadge: "S3/S4", 
+        type: "Supply & FE",
+        scheme: "2019 Scheme",
+        startDate: "2026-07-10T09:30:00", 
+        endDate: "2026-07-27T12:30:00",   
+        viewLink: "btech/s4/supply-2026.html",
+        pdfLink: "https://drive.google.com/file/d/1Ndiq-v27ol7_42OG_Y42YedyBaQruOyz/view?usp=drivesdk"
+    }
+];
