1cbe97038cfdfbf4f358842e9ed0891f4bff892c
KeralaTimetable <sudhasangeethveena@gmail.com>
2026-05-25 11:08:52 +0530

    Update timetables.py

--- timetables.py
+++ timetables.py
@@ -261,7 +261,7 @@ def extract_dashboard_data_with_ai(pdf_path, original_title, pdf_link, html_link
     
     try:
         response = client.models.generate_content(
-            model='gemini-3.5-flash',
+            model='gemini-3.1-flash-lite',
             contents=prompt,
             config=types.GenerateContentConfig(
                 response_mime_type="application/json",
